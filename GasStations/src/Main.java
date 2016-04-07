8154b6afac3d8b8cd8fb17ce1f05308650d522b7
Oliver Kim <oliver94s@yahoo.com>
2016-04-07 15:47:00 -0700

    adding file parsing

--- GasStations/src/Main.java
+++ GasStations/src/Main.java
@@ -1,19 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author olive_000
  */
 public class Main {
-   
-   
-   
-   
+
+   public void readFile(String fileName) {
+      try {
+         FileReader fileReader = new FileReader(fileName);
+
+         BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+         String line = bufferedReader.readLine();
+         
+         while (line != null){
+            
+         }
+      }
+      catch (Exception ex) {
+         System.out.println("hey");
+      }
+
+   }
+
    public static void main(String[] args) {
-      
+
    }
 }
