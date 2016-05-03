4a98d4f5fdc609a1074539e097660dc572863a3c
Oliver Kim <oliver94s@yahoo.com>
2016-05-03 00:52:10 -0700

    Turned in the assignment, not entirely sure I know how many situations I cover actually

--- MaxSat/src/Main.java
+++ MaxSat/src/Main.java
@@ -47,7 +47,7 @@ public class Main {
         } catch (FileNotFoundException ex) {
             System.out.println("hey");
         }
-
+        int total = 0;
         int[] elements = new int[1001];
         boolean cont = true;
 
@@ -74,6 +74,7 @@ public class Main {
                     elementNum = i;
                 }
             }
+            total += count;
 
             if (elementNum < 500) {
                 elementNum = elementNum * -1;
@@ -106,38 +107,56 @@ public class Main {
                 }
             }
 
-            int empty = 0;
-            for (ArrayList<Integer> x : literals.values()) {
-                if (x.isEmpty()) {
-                    empty++;
-                }
-            }
-            System.out.println(empty + "\t" + values.size());
-            if (values.size() > 25) {
-                for (int i = 0; i < values.size(); i++) {
-                    for (int j = i + 1; j < values.size(); j++) {
-                        if (values.get(i) == values.get(j)) {
-                            System.out.println("Same Value");
-                            break;
-                        } else if (values.get(j) == 0) {
-                            System.out.println("Zero");
-                            break;
-                        } else if (values.get(i) == values.get(j) * -1) {
-                            System.out.println("Negative");
-                            break;
-                        }
-                    }
-                }
-            }
+//            int empty = 0;
+//            for (ArrayList<Integer> x : literals.values()) {
+//                if (x.isEmpty()) {
+//                    empty++;
+//                }
+//            }
+//            System.out.println(total + "\t" + values.size());
+//            if (values.size() > 25) {
+//                for (int i = 0; i < values.size(); i++) {
+//                    for (int j = i + 1; j < values.size(); j++) {
+//                        if (values.get(i) == values.get(j)) {
+//                            System.out.println("Same Value");
+//                            break;
+//                        } else if (values.get(j) == 0) {
+//                            System.out.println("Zero");
+//                            break;
+//                        } else if (values.get(i) == values.get(j) * -1) {
+//                            System.out.println("Negative");
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
             if (values.size() == 500) {
                 cont = false;
             }
         }
 
+//        for (int i = 0; i < elements.length; i++) {
+//            System.out.println(i + " " + elements[i]);
+//        }
+//        System.out.println("");
+
+        int[] check = new int[501];
+        for (int x : values) {
+            if (x > 0) {
+                check[Math.abs(x)]++;
+            }
+        }
+        for (int i = 0; i < 501; i++) {
+//            if (check[i] == 0) {
+//                System.out.println(i);
+//            }
+            System.out.println(i + " " + check[i]);
+        }
+
         PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
 
-        for (int x : values) {
-            writer.println(x);
+        for (int x : check) {
+            writer.print(x);
         }
 
         writer.close();
